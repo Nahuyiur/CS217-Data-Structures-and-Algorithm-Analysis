4095ae4a5dccbbec52b18bf71902693956fb1abd
芮煜涵 <150534350+Nahuyiur@users.noreply.github.com>
2024-12-02 02:14:23 +0800

    finish t1

--- Lab11/Memoized-Cut-Rod.cpp
+++ Lab11/Memoized-Cut-Rod.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-int doCal(int[] &p,int n,int[] &r,int[] &s){
-    int q=0;
-    if(r[n]>=0)return r[n];
+int doCal(int* p,int n,int* r,int* s){
+    int q=-1;
+    if (n < 0) return 0;  // 防止非法递归调用
+    if(r[n]>0)return r[n];
     if(n==0){
         q=0;s[n]=0;
     }else{
         q=-1;
-        s=0;
+        s[n]=0;
         for(int i=1;i<=n;i++){
-            if(q<p[i]+doCal(p,n-i,r,s)){
-                q=p[i]+doCal(p,n-i,r,s);
-                s[n]=i;
+            int temp = doCal(p, n - i, r, s);
+            if (q < p[i] + temp) {
+                q = p[i] + temp;
+                s[n] = i;
             }
         }
     }
@@ -25,6 +28,8 @@ int main(){
     int p[n+1];
     int r[n+1];
     int s[n+1];
+
+    p[0]=0,r[0]=0,s[0]=0;
     for(int i=1;i<=n;i++){
         cin>>p[i];
         r[i]=-1;
@@ -32,21 +37,22 @@ int main(){
     }
 
     
-    for(int i=1;i<=n;i++){
-        r[i]=doCal(p,n,r,s); 
-    }
+    doCal(p,n,r,s);
 
     cout<<r[n]<<endl;
 
     vector<int> vec;
-    while(n!=0){
+    while(n>0){
         vec.push_back(s[n]);
         n-=s[n];
     }
+
     cout<<vec.size()<<endl;
 
     std::sort(vec.begin(), vec.end());
-    for (int n : vec) std::cout << n << " ";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
     cout<<endl;
     return 0;
 }
